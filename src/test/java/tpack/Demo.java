c8a80f8406d4cccf7d73f6d3cd98b4cdfdc59d6f
deepakchaudhari9999 <deepakchaudhari9999@gmail.com>
2024-03-12 15:05:36 +0530

    First commit....

---
+++ src/test/java/tpack/Demo.java
@@ -0,0 +1,32 @@
+package tpack;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo {
+	WebDriver driver;
+
+	@Test
+	public void mOne() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.get("https://web.whatsapp.com/");
+
+		driver.manage().window().maximize();
+
+	}
+
+	
+
+}
